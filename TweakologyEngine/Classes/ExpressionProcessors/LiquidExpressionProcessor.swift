3177a9d1f7d10a356d462ce23720e8652acebb18
Nikolay Ivanov <velikolay@gmail.com>
2020-02-15 01:35:52 +0200

    Fix whitespace issues with liquid

--- TweakologyEngine/Classes/ExpressionProcessors/LiquidExpressionProcessor.swift
+++ TweakologyEngine/Classes/ExpressionProcessors/LiquidExpressionProcessor.swift
@@ -10,7 +10,12 @@ import Foundation
 class LiquidExpressionProcessor: ExpressionProcessor {
     func process(expression: String, context: [String: Any]) -> String? {
         let lexer = Lexer(templateString: expression)
-        let tokens = lexer.tokenize()
+        let tokens = lexer.tokenize().map { (token) -> Token in
+            if case .text(let value) = token {
+                return .text(value: value.trimmingCharacters(in: .newlines))
+            }
+            return token
+        }
         let parser = Parser(tokens: tokens, context: Context(dictionary: context))
         let exprValues = parser.parse()
         return exprValues.joined(separator: "")
